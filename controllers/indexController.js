7be843073a76f82ab0489a599874c4df160386d1
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-14 17:50:55 +0530

    Add new controller functions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,30 @@
 const getHome = (req, res) => {
     const locals = { title: "Home | RecipeNest" };
-    return res.render("index", {
+    return res.status(200).render("index", {
+        locals,
+        layout: "layouts/mainLayout",
+    });
+};
+
+const getContact = (req, res) => {
+    const locals = { title: "Contact Us | RecipeNest" };
+    return res.status(200).render("contact", {
+        locals,
+        layout: "layouts/mainLayout",
+    });
+};
+
+const getRecipes = (req, res) => {
+    const locals = { title: "Recipes | RecipeNest" };
+    return res.status(200).render("recipes", {
+        locals,
+        layout: "layouts/mainLayout",
+    });
+};
+
+const getRecipeDetails = (req, res) => {
+    const locals = { title: "Recipe Details | RecipeNest" };
+    return res.status(200).render("recipeDetails", {
         locals,
         layout: "layouts/mainLayout",
     });
@@ -8,4 +32,7 @@ const getHome = (req, res) => {
 
 module.exports = {
     getHome,
-}
+    getContact,
+    getRecipes,
+    getRecipeDetails,
+};
