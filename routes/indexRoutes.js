cb859276ead5e378190bc18ce3ef25e6e89dd8d5
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-14 17:52:12 +0530

    Add new routes and client side contact form validation

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.getHome);
+router.get('/contact', indexController.getContact);
+router.get('/recipes', indexController.getRecipes);
+router.get('/recipes/recipe-details', indexController.getRecipeDetails);
 
 module.exports = router;
