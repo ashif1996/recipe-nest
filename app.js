10b21c85ee15c77d22a304f35024c203121b6ec6
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-14 19:52:16 +0530

    Set MongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
+const connectToDatabase = require("./config/dbConfig");
+connectToDatabase();
 
 const express = require("express");
 const path = require("path");
