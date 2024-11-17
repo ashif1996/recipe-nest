f0f5453196876e8d9a3e308aa109d69dd57f5c6b
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-17 13:59:23 +0530

    Add otp model

---
+++ models/otpModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const otpSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    otp: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => /\d{6}/.test(value),
+            message: "OTP must be a 6-digit number",
+        },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+    }
+});
+
+const OTP = mongoose.model("OTP", otpSchema);
+
+module.exports = OTP;
