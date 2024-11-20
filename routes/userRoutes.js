3669358a4fca4b0b35e3f26b9dd9c82d787909da
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-20 08:27:00 +0530

    Add category routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,26 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 const authenticateJWT = require("../middlewares/jwtMiddleware");
+const upload = require("../config/multerConfig");
 
 router.route("/login")
     .get(userController.getUserLogin)
     .post(userController.userLogin);
+
 router.route("/signup")
     .get(userController.getUserSignup)
     .post(userController.userSignup);
+
 router.get('/logout', userController.userLogout);
+
 router.get('/favourite-recipes', authenticateJWT, userController.getFavouriteRecipes);
-router.get("/add-recipe", authenticateJWT, userController.getAddRecipe);
-router.get("/add-category", authenticateJWT, userController.getAddCategory);
+
+router.route("/categories/add")
+    .get(authenticateJWT, userController.getAddCategory)
+    .post(authenticateJWT, upload, userController.addCategory);
+
+router.route("/add-recipe")
+    .get(authenticateJWT, userController.getAddRecipe)
+    .post(authenticateJWT, upload, userController.addRecipe);
 
 module.exports = router;
