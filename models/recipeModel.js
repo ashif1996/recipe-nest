6977b192c83762d28c3daa52e1235601c88f213e
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-25 17:23:45 +0530

    Update recipe model

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -11,7 +11,7 @@ const recipeSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        maxlength: 50,
+        maxlength: 200,
     },
     image: {
         type: String,
@@ -45,29 +45,10 @@ const recipeSchema = new Schema({
             message: "Steps cannot be empty",
         },
     },
-    similarRecipes: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Recipe",
-        }
-    ],
     createdAt: {
         type: Date,
         default: Date.now,
     },
-});
-
-recipeSchema.pre("save", async function(next) {
-    if (this.isNew) {
-        const similarRecipes = await this.constructor.find({
-            category: this.category,
-            _id: { $ne: this.id },
-        }).limit(4);
-
-        this.similarRecipes = similarRecipes.map(recipe => recipe._id);
-    }
-
-    next();
 });
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
