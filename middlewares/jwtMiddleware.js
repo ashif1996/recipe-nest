a7e9019f30335bcd12ab3fe93b527ff364c7735a
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-25 17:38:31 +0530

    Update jwt middleware

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -1,24 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateJWT = (req, res, next) => {
-    try {
-        const token = req.cookies.authToken || req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies.authToken || req.header("Authorization")?.replace("Bearer ", "");
 
-        if (!token) {
-            req.flash("error", "Access denied. Please log in.");
+    if (!token) {
+        req.flash("error", "Access denied. Please log in.");
+        return res.redirect("/users/login");
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            if (err.name === "TokenExpiredError") {
+                req.flash("error", "Session has expired. Please log in again.");
+                return res.redirect("/users/login");
+            }
+
+            req.flash("error", "Invalid token. Please log in again.");
             return res.redirect("/users/login");
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-
         next();
-    } catch (error) {
-        console.error("Error verifying the token:", error);
-
-        req.flash("error", "Session expired. Please log in again.");
-        return res.redirect("/users/login");
-    }
+    });    
 };
 
 module.exports = authenticateJWT;
