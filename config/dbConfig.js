ee4e8aaf083ea2d4336e30640bc8413fe787de3e
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-27 21:32:02 +0530

    update database configuration

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,6 +5,7 @@ const connectToDatabase = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI);
         const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
+        
         console.log(`MongoDB connected: ${connectionDetails}`);
     } catch (error) {
         console.log(`Database connection error: ${error.message}`);
