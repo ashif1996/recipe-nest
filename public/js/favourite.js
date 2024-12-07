124f4dce0f98320715f30c271fc74764968404bb
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-07 21:52:36 +0530

    Remove sweet alert

--- public/js/favourite.js
+++ public/js/favourite.js
@@ -13,52 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    credentials: "include", // Ensures cookies are sent
+                    credentials: "include",
                     body: JSON.stringify({ recipeId }),
                 });
 
-                const result = await response.json(); // Parse the JSON response
+                const result = await response.json();
 
                 if (response.status === 401) {
-                    // Handle 401 Unauthorized response (for logged-out users)
-                    Swal.fire({
-                        icon: 'info',
-                        title: 'Error',
-                        text: result.message,
-                        confirmButtonText: 'OK',
-                    }).then(() => {
-                        window.location.href = "/users/login";  // Redirect to login page
-                    });
+                    window.location.href = `/recipes/${recipeId}`;
                     return;
                 }
 
-                if (result.ok) {
-                    // Success case
-                    Swal.fire({
-                        icon: 'success',
-                        title: result.message,
-                        confirmButtonText: 'OK',
-                    });
+                if (result.success) {
+                    window.location.href = `/recipes/${recipeId}`;
+                    return;
                 } else {
-                    // Error case
-                    Swal.fire({
-                        icon: 'info',
-                        title: 'Error',
-                        text: result.message,
-                        confirmButtonText: 'OK',
-                    });
+                    window.location.href = `/recipes/${recipeId}`;
+                    return;
                 }
-
             } catch (error) {
                 console.error("Error adding recipe to favorites:", error);
-
-                // Handle errors from the fetch call (network errors, etc.)
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: "An error occurred while adding the recipe to favorites.",
-                    confirmButtonText: 'OK',
-                });
+                window.location.href = `/recipes/${recipeId}`;
             }
         });
     }
