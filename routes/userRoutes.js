1db7bd351efc2dc0d8e92daabde8dd77dbcb6a7b
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-07 08:21:05 +0530

    Update user related routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -23,9 +23,17 @@ router.route("/add-category")
     .get(authenticateJWT, userController.getAddCategory)
     .post(authenticateJWT, upload, userController.addCategory);
 
+router.route("/edit-category/:id")
+    .get(authenticateJWT, userController.getEditCategory)
+    .put(authenticateJWT, upload, userController.editCategory);
+
 router.route("/add-recipe")
     .get(authenticateJWT, userController.getAddRecipe)
     .post(authenticateJWT, upload, userController.addRecipe);
+
+router.route("/edit-recipe/:id")
+    .get(authenticateJWT, userController.getEditRecipe)
+    .put(authenticateJWT, upload, userController.editRecipe);
 
 router.post("/contact/send-email", userController.processSendEmail);
 
