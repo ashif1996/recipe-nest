8655032ed429340818cd1c91686df5f8ee259ac5
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-07 20:45:20 +0530

    Add showFlashMessages utility function

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+const showFlashMessages = require("../utils/messageUtils");
 const httpStatusCode = require("../utils/httpStatusCode");
 
 // Middleware to authenticate JWT Tokens
@@ -7,19 +8,37 @@ const authenticateJWT = (req, res, next) => {
     const token = req.cookies.authToken || req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-        req.flash("error", "Access denied. Please log in.");
-        return res.status(httpStatusCode.UNAUTHORIZED).redirect("/users/login");
+        return showFlashMessages({
+            req,
+            res,
+            type: "error",
+            message: "Access denied. Please log in.",
+            status: httpStatusCode.UNAUTHORIZED,
+            redirectUrl: "/users/login",
+        });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             if (err.name === "TokenExpiredError") {
-                req.flash("error", "Session has expired. Please log in again.");
-                return res.status(httpStatusCode.UNAUTHORIZED).redirect("/users/login");
+                return showFlashMessages({
+                    req,
+                    res,
+                    type: "error",
+                    message: "Session has expired. Please log in again.",
+                    status: httpStatusCode.UNAUTHORIZED,
+                    redirectUrl: "/users/login",
+                });
             }
 
-            req.flash("error", "Invalid token. Please log in again.");
-            return res.status(httpStatusCode.UNAUTHORIZED).redirect("/users/login");
+            return showFlashMessages({
+                req,
+                res,
+                type: "error",
+                message: "Invalid token. Please log in again.",
+                status: httpStatusCode.UNAUTHORIZED,
+                redirectUrl: "/users/login",
+            });
         }
 
         req.user = decoded;
