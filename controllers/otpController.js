826aba161cc403e929bd092447dd82043781abe4
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-14 13:38:45 +0530

    Add helper function for http status codes

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -6,7 +6,7 @@ const {
     cleanupExpiredOtps
 } = require("../utils/otpUtils");
 
-const HttpStatus = require("../utils/httpStatusCode");
+const HttpStatusCode = require("../utils/httpStatusCode");
 
 // Handles sending an OTP to the user's email address
 const sendOtpToUser = async (req, res) => {
@@ -18,19 +18,20 @@ const sendOtpToUser = async (req, res) => {
         const otp = generateOtp();
         console.log("Generated OTP:", otp);
         await storeOtp(email, otp);
+
         await sendOtp(email, otp);
 
         req.session.otpSend = true;
         req.session.email = email;
 
-        return res.status(HttpStatus.OK).json({
+        return res.status(HttpStatusCode.OK).json({
             success: true,
             otpSend: true,
             message: "OTP sent successfully.",
             redirectUrl,
         });
     } catch (error) {
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
             success: false,
             otpSend: false,
             message: error.message || "Failed to send OTP.",
@@ -49,19 +50,25 @@ const handleOtpVerification = async (req, res) => {
         if (result.isVerified) {
             req.session.otpSend = false;
 
-            return res.status(HttpStatus.OK).json({
+            return res.status(HttpStatusCode.OK).json({
                 success: true,
                 message: "OTP verified successfully.",
                 redirectUrl,
             });
         } else {
-            const message = result.reason === "expired" ? "The OTP has expired." : "Invalid OTP.";
-            return res.status(HttpStatus.BAD_REQUEST).json({ success: false, message });
+            const message = result.reason === "expired"
+                ? "The OTP has expired."
+                : "Invalid OTP.";
+
+            return res.status(HttpStatusCode.BAD_REQUEST).json({
+                success: false,
+                message
+            });
         }
     } catch (error) {
         console.error("Error verifying OTP: ", error);
 
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
             success: false,
             message: "An error occurred while verifying OTP.",
             redirectUrl,
